41bbc64d56934e29e71aceae2180a0ea79f8c5e8
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-08 10:25:37 -0800

    WIP: path fix.

--- BestRestaurant/Controllers/CuisinesController.cs
+++ BestRestaurant/Controllers/CuisinesController.cs
@@ -1,23 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
-using BestRestaurant.Models;
-using System.Collections.Generic;
-using System.Linq;
+// using Microsoft.AspNetCore.Mvc;
+// using BestRestaurant.Models;
+// using System.Collections.Generic;
+// using System.Linq;
 
-namespace BestRestaurant.Controllers
-{
-  public class CuisinesController : Controller
-  {
-    private readonly BestRestaurantContext _db;
+// namespace BestRestaurant.Controllers
+// {
+//   public class CuisinesController : Controller
+//   {
+//     private readonly BestRestaurantContext _db;
 
-    public CuisinesController(BestRestaurantContext db)
-    {
-      _db = db;
-    }
+//     public CuisinesController(BestRestaurantContext db)
+//     {
+//       _db = db;
+//     }
 
-    public ActionResult Index()
-    {
-      List<Cuisine> model = _db.Cuisines.ToList();
-      return View(model);
-    }
-  }
-}
+//     public ActionResult Index()
+//     {
+//       List<Cuisine> model = _db.Cuisines.ToList();
+//       return View(model);
+//     }
+//   }
+// }
